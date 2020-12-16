12980eb33089bd3d526098bbec7f05b33f97b79e
kage1414 <kylejohnson92294@gmail.com>
2020-12-16 09:47:30 -0500

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,34 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = { 'length': 0 };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (storage.length > 0) {
+      for (var i = storage.length - 1; i >= 0; i--) {
+        someInstance[i + 1] = someInstance[i];
+      }
+    }
+    someInstance[0] = value;
+    storage.length += 1;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = someInstance[storage.length - 1];
+    delete someInstance[storage.length];
+    storage.length -= 1;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    var count = 0;
+
+    for (var i = 0; i < storage.length; i++) {
+      count += 1;
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,36 +1,39 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {'length': 0};
 
   // Use an object with numeric keys to store values
-  var storage = {'length': 0};
+  var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
-    if (storage.length === 0) {
+    if (someInstance.length === 0) {
       someInstance[0] = value;
     } else {
-      for (var i = storage.length; i > 0; i--) {
+      for (var i = someInstance.length; i > 0; i--) {
         someInstance[i] = someInstance[i - 1];
       }
+      someInstance[0] = value;
     }
-    storage.length += 1;
+    someInstance.length += 1;
   };
 
   someInstance.pop = function() {
     var popped = someInstance[0];
 
-    for (var i = 0; i < storage.length; i++) {
+    for (var i = 0; i < someInstance.length; i++) {
       someInstance[i] = someInstance[i + 1];
     }
 
-    storage.length -= 1;
+    delete someInstance[someInstance.length];
+
+    someInstance.length -= 1;
     return popped;
 
   };
 
   someInstance.size = function() {
     var count = 0;
-    for (var i = 0; i < storage.length; i++) {
+    for (var i = 0; i < someInstance.length; i++) {
       count += 1;
     }
     return count;
