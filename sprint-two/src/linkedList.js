146a9bdc31905e9bb30c8b78e67da9b8e79bb81f
kage1414 <kylejohnson92294@gmail.com>
2021-01-04 20:06:00 -0500

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create new instance of node
+    var newNode = Node(value);
+    // if no head
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // assign a temporary variable to the head
+    var oldHead = list.head.value;
+    list.head = list.head.next;
+    return oldHead;
   };
 
   list.contains = function(target) {
+    // check values of both the head and the tail first
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    }
+
+    // create currentNode variable (set to head at first)
+    var currentNode = list.head;
+
+    // checks everything inbetween head and tail
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      // change currentNode to the next node
+      if (currentNode.value === target) {
+        return true;
+      }
+    }
+    // return false otherwise
+    return false;
   };
 
   return list;
