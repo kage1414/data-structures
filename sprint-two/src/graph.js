a40444349b76814f79b2e6611d47fb950d0c80e6
kage1414 <kylejohnson92294@gmail.com>
2021-01-06 00:03:42 -0500

    Remove .toString() from graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -16,7 +16,7 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[node.toString()] = [];
+  this.nodes[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
