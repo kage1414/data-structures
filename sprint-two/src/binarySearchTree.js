f64261897f870dd7b55ce5980eb9fa8fb8bf6830
kage1414 <kylejohnson92294@gmail.com>
2021-01-09 13:09:33 -0600

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,63 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(val) {
+  var storage = {};
+  storage.value = val;
+  storage.left = null;
+  storage.right = null;
+  _.extend(storage, bstTreeMethods);
+
+  return storage;
 };
 
+var bstTreeMethods = {
+  insert: function (val) {
+    if (val < this.value) {
+      if (this.left) {
+        this.left.insert(val);
+      } else {
+        this.left = BinarySearchTree(val);
+      }
+    } else if (val > this.value) {
+      if (this.right) {
+        this.right.insert(val);
+      } else {
+        this.right = BinarySearchTree(val);
+      }
+    }
+  },
+  contains: function(val) {
+    if (this.value === val) {
+      return true;
+    } else if (val < this.value && this.left !== null) {
+      return this.left.contains(val);
+    } else if (val > this.value && this.right !== null) {
+      return this.right.contains(val);
+    }
+
+    return false;
+  },
+  depthFirstLog: function(callback) {
+    callback(this.value);
+    if (this.left) {
+      this.left.depthFirstLog(callback);
+    }
+    if (this.right) {
+      this.right.depthFirstLog(callback);
+    }
+  }
+
+};
+
+// .insert = first value top
+
+// var newBST = BinarySearchTree(8)
+// newBST.dephFirstLog(callback)
+
+
+//     5
+// 2       7
+//   3   6
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
